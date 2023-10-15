2ba85f8fd11cfc737b84cd3e0cdc39e807b29a7a
Alexsand74 <1974alexsand@gmail.com>
2023-10-15 16:10:43 +0300

    Homework 3.3 introduction to databases will be located here.

---
+++ src/main/java/com/example/homework3_3_introductiontodatabases/Homework33IntroductionToDatabasesApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework3_3_introductiontodatabases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework33IntroductionToDatabasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework33IntroductionToDatabasesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework3_3_introductiontodatabases/Homework33IntroductionToDatabasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework3_3_introductiontodatabases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework33IntroductionToDatabasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
